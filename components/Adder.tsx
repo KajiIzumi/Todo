48b2b39df1bf7cda640f650644211ba7e0c1e39a
KajiIzumi <19t2039a@shinshu-u.ac.jp>
2023-01-05 16:10:25 +0900

    ✨ Step6・数字が増える機能を追加

--- components/Adder.tsx
+++ components/Adder.tsx
@@ -2,14 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export function Adder() {
+  const [cnt, setCnt] = useState(0);
+
+  const handleClick = () => {
+    setCnt((cnt) => cnt + 1);
+  };
+
   return (
     <>
-      <div className="text-center">num</div>
-      <button className="flex flex-col items-center">addButton</button>
+      <div className="text-center">{cnt}</div>
+      <button onClick={handleClick}>addButton</button>
     </>
   );
 }
