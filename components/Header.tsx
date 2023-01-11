35e5c6a456cd29cbe3515ccc7daa87aeafea2dd9
KajiIzumi <19t2039a@shinshu-u.ac.jp>
2023-01-11 16:09:45 +0900

    🎨 typescript宣言の不要部分を削除

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,35 +1,10 @@
 import Head from "next/head";
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from "react";
 
-export default function Header(props: {
-  stepNum:
-    | string
-    | number
-    | boolean
-    | ReactElement<any, string | JSXElementConstructor<any>>
-    | ReactFragment
-    | ReactPortal
-    | null
-    | undefined;
-  title:
-    | string
-    | number
-    | boolean
-    | ReactElement<any, string | JSXElementConstructor<any>>
-    | ReactFragment
-    | ReactPortal
-    | null
-    | undefined;
-}) {
+export default function Header(props: { stepNum: number; title: string }) {
   return (
     <>
       <Head>
-        <title>Step{props.stepNum}</title>
+        <title>{`Step${props.stepNum}`}</title>
       </Head>
       <h1 className="text-center">
         <div className="text-2xl p-5">Step{props.stepNum}</div>
