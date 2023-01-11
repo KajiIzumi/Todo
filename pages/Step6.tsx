9f14c29f2e984439f46dca12ce1e168608cc202b
KajiIzumi <19t2039a@shinshu-u.ac.jp>
2023-01-11 16:06:10 +0900

    🎨

--- pages/Step6.tsx
+++ pages/Step6.tsx
@@ -1,11 +1,8 @@
-import Head from "next/head";
-import Link from "next/link";
 import { Adder } from "../components/Adder";
 import { GoTop } from "../components/GoTop";
 import Header from "../components/Header";
 
 export default function Step6() {
-  //プルリクエストテスト
   return (
     <>
       <main>
